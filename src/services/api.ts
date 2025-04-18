935402af8e2e0841bda877360046690768af71db
Caio Souza <kcaiosouza@gmail.com>
2025-04-18 13:48:48 -0300

    hotfix(MNKT-0010): new api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL ||  'https://dumazdevback.loca.lt',
-  headers: {
-    'bypass-tunnel-reminder': '*',
-  },
+  baseURL: process.env.API_URL ||  'https://8fc9-2804-29b8-507a-a7fe-1de5-71c5-6081-a805.ngrok-free.app',
 })
