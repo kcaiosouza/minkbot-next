5f252308e31ff3a2d72f8ce7a3835f39dfc8deb1
Caio Souza <kcaiosouza@gmail.com>
2025-04-25 01:33:24 -0300

    hotfix: nova api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL ||  'https://8fc9-2804-29b8-507a-a7fe-1de5-71c5-6081-a805.ngrok-free.app',
+  baseURL: process.env.API_URL ||  'https://3c9a-2804-29b8-507a-a7fe-6870-3acc-34d1-acc9.ngrok-free.app',
 })
