8f23bd98e339e7857bdbf7d3d5ef79655f2aecbb
Caio Souza <kcaiosouza@gmail.com>
2025-06-11 18:57:17 -0300

    hotfix: new api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL ||  'https://9300-2804-29b8-507a-a7fe-190-99e5-dfc3-ca15.ngrok-free.app',
+  baseURL: process.env.API_URL ||  'https://5e26-2804-29b8-507a-d103-dcb5-ece3-2309-4532.ngrok-free.app',
 })
